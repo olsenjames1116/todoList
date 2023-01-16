fa01547d9e520d291f97f13cdbea491c57d0a8cf
James Olsen <olsenjames1116@gmail.com>
2023-01-16 11:48:12 -0700

    Create home module

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
@@ -9,8 +10,5 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
